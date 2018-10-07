8cd5ab056516d9c9a920dc928edfc9db7141b4a4
Jessica <jessalatorre.f@gmail.com>
2018-10-06 21:06:12 -0500

    Listo el primer ejercicio usando sklearn

--- project1.py
+++ project1.py
@@ -1,12 +1,10 @@
 # Importing various packages
-from random import random, seed
+from random import random
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import pandas as pd
-from pandas import DataFrame
 from sklearn.preprocessing import PolynomialFeatures
-
+from sklearn import metrics
+import pandas as pd
 
 #Funcion que nos regresa el set de la variable dependiente
 def FrankeFuction(x, y):
@@ -14,11 +12,12 @@ def FrankeFuction(x, y):
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    noise = np.random.randn(numData,1)
-    return term1 + term2 + term3 + term4 + noise
+    noise = np.random.randn(numData,1)  ##Adding some noise 
+    return term1 + term2 + term3 + term4 + (0.01*noise)
 
 #data
-numData = 50
+numData = 500
+gradoPoly = 5
 
 # x & y son las variables predictos
 x = np.random.rand(numData, 1)
@@ -26,33 +25,87 @@ y = np.random.rand(numData, 1)
 z = FrankeFuction(x, y)
 
 dataset = pd.DataFrame({'predicto1': list(x), 'predicto2': list(y), 'outcome' : list(z)}, columns = ['predicto1', 'predicto2', 'outcome'])
-#print(dataset.head())
 
-print(dataset)
+#print(dataset)
 indep = dataset[['predicto1', 'predicto2']]
 depen = dataset['outcome']
 
-regr = LinearRegression()  #Linear regression object
-regr.fit(indep, depen)
+#Using Linear Regression
+poly = PolynomialFeatures(degree=gradoPoly)
+indep_= poly.fit_transform(indep)
+regrGr = LinearRegression()
+regrGr.fit(indep_, depen)
+intercept  = regrGr.intercept_
+beta = regrGr.coef_
+depen_pred = regrGr.predict(indep_)
 
-print('Intercept: \n', regr.intercept_)
-print('Coefficients: \n', regr.coef_)
-
-
-poly5 = PolynomialFeatures(degree=5)
-PREDS = poly5.fit_transform(x[:,np.newaxis])
-
-clf5 = LinearRegression()
-clf5.fit(PREDS, depen)
-
-"""
-print('ypredict: ', ypredict)
-plt.plot(xnew, ypredict, "r-")
-plt.plot(x, y ,'ro')
-plt.axis([0,2.0,0, 15.0])
-plt.xlabel(r'$x$')
-plt.ylabel(r'$y$')
-plt.title(r'Random numbers ')
-plt.show()
+bias = np.sum( (depen- np.mean(depen_pred))**2 )/numData
+print('Multiple regression model using the whole data:')
+print('Beta parameters:', beta)
+print('Mean Squared Error (MSE): ', metrics.mean_squared_error(depen, depen_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(depen, depen_pred)))
+print('Variance: ', np.var(depen_pred))
+print('Bias:', bias)
 
 """
+error = np.mean( np.mean((depen - depen_pred)**2) )
+bbias = np.mean( (depen - np.mean(depen_pred))**2 )
+vvariance = np.mean( np.var(depen_pred) )
+print('Error:', error)
+print('Bias^2:', bbias)
+print('Var:', vvariance)
+print('{} >= {} + {} = {}'.format(error, bbias, vvariance, bbias+vvariance))
+"""
+
+print('\n\n------Splitting between train and test data --------')
+#Evaluating model using cross-validation resample technique
+from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
+indep_train, indep_test, dep_train, dep_test = train_test_split(indep, depen, test_size=0.2)
+
+betaMayor = []
+numExp = 10
+MSE_ = 1.0
+for i in range(0,numExp): #Number of experiments
+    indep_trainR, dep_trainR = resample(indep_train, dep_train)
+    #20% of the data is the test set (given by test_size=0.2)
+    poly = PolynomialFeatures(degree=gradoPoly)
+    indep_trainR_ = poly.fit_transform(indep_trainR)
+    regrGr = LinearRegression()
+    regrGr.fit(indep_trainR_, dep_trainR)
+    betaS = regrGr.coef_
+    betaMayor = np.concatenate((betaMayor, betaS)) #Getting all the values of B
+    #Making predictions
+    indep_test_ = poly.fit_transform(indep_test) #using the test data as polynomial 
+    dep_pred = regrGr.predict(indep_test_)
+    #Calculating MSE, R2, Var and Bias for each experiment
+      
+    MSE = np.mean( np.mean((depen - dep_pred)**2) )
+    R2 = np.sqrt(np.mean( np.mean((depen - dep_pred)**2) ))
+    bias = np.mean( (depen - np.mean(dep_pred))**2 )
+    variance = np.mean( np.var(dep_pred) )   
+   
+    if (MSE <= MSE_):
+        #Saving the values for the model with less MSE
+        Beta_= betaS
+        MSE_ = MSE
+        R2_= R2
+        var_ = variance
+        bias_ = bias
+        
+#Getting a Matrix with all the values from B parameters of each experiment    
+BetaMatrix = betaMayor.reshape(numExp, len(betaS))
+print('Variances for each beta parameters: ', np.var(BetaMatrix,0) )
+print('\nConfidence intervals of the parameters beta: (95%)')
+for i in range(0, len(betaS)):
+    print("B_", i, " {}".format(np.percentile(BetaMatrix[:,i], [2.5, 97.5])))
+
+print('\n--Selecting the model with less MSE--')
+print('Beta parameters:', Beta_)
+print('Mean Squared Error (MSE): ', MSE_)
+print('Root Mean Squared Error:', R2_)
+print('Variance: ',var_)
+print('Bias:', bias_)
+print('Var + Bias =', var_ + bias_)
+
+
--- pruebas.py
+++ pruebas.py
@@ -1,68 +1,147 @@
-"""import numpy as np
-import pandas as pd
-from pandas import DataFrame
-
-
-numData=20
-x = np.random.rand(numData, 1)
-y = np.random.rand(numData, 1)
-print(x)
-print(y)
-
-dataset = pd.DataFrame({'predicto1': list(x), 'predicto2': list(y)}, columns = ['predicto1', 'predicto2'])
-
-print(dataset.head())"""
-
-
-
-from random import random, seed
 import numpy as np
+#from scipy.misc import imread
+from imageio import imread
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-import pandas as pd
-from pandas import DataFrame
-from sklearn.preprocessing import PolynomialFeatures
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
+def surface_plot(surface,title, surface1=None):
+    M,N = surface.shape
+    ax_rows = np.arange(M)
+    ax_cols = np.arange(N)
+    [X,Y] = np.meshgrid(ax_cols, ax_rows)
+    fig = plt.figure()
+    if surface1 is not None:
+        ax = fig.add_subplot(1,2,1,projection='3d')
+        ax.plot_surface(X,Y,surface,cmap=cm.viridis,linewidth=0)
+        plt.title('Real data')
 
-#X is the independent variable (bivariate in this case)
+        ax = fig.add_subplot(1,2,2,projection='3d')
+        ax.plot_surface(X,Y,surface1,cmap=cm.viridis,linewidth=0)
+        plt.title(title)
+    else:
+        ax = fig.gca(projection='3d')
+        ax.plot_surface(X,Y,surface,cmap=cm.viridis,linewidth=0)
+        plt.title(title)
 
-
-#vector is the dependent data
-vector = [109.85, 155.72]
-
-#predict is an independent variable for which we'd like to predict the value
-predict= [0.49, 0.18]
-
-#generate a model of polynomial features
-poly = PolynomialFeatures(degree=2)
-
-#transform the x data for proper fitting (for single variable type it returns,[1,x,x**2])
-#X_ = poly.fit_transform(X)
-
-#Funcion que nos regresa el set de la variable dependiente
-def FrankeFuction(x, y):
+def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-    noise = np.random.randn(numData,1)
-    return term1 + term2 + term3 + term4 + noise
+    return term1 + term2 + term3 + term4
 
-#data
-numData = 50
+def my_polyfit(num_data, poly_deg, x, y):
+    if poly_deg == 1:
+        data_vec = np.c_[np.ones((num_data,1)), x, y]
+    elif poly_deg == 2:
+        data_vec = np.c_[np.ones((num_data,1)), x, y, x**2, x*y, y**2]
+    elif poly_deg == 3:
+        data_vec = np.c_[np.ones((num_data,1)), x, y, x**2, x*y, y**2, \
+                            x**3, x**2*y, x*y**2, y**3]
+    elif poly_deg == 4:
+        data_vec = np.c_[np.ones((num_data,1)), x, y, x**2, x*y, y**2, \
+                            x**3, x**2*y, x*y**2, y**3, \
+                            x**4, x**3*y, x**2*y**2, x*y**3,y**4]
+    elif poly_deg == 5: 
+        data_vec = np.c_[np.ones((num_data,1)), x, y, x**2, x*y, y**2, \
+                            x**3, x**2*y, x*y**2, y**3, \
+                            x**4, x**3*y, x**2*y**2, x*y**3,y**4, \
+                            x**5, x**4*y, x**3*y**2, x**2*y**3,x*y**4,y**5]
+    return data_vec
 
-# x & y son las variables predictos
-x = np.random.rand(numData, 1)
-y = np.random.rand(numData, 1)
-z = FrankeFuction(x, y)
 
-dataset = pd.DataFrame({'predicto1': list(x), 'predicto2': list(y), 'outcome' : list(z)}, columns = ['predicto1', 'predicto2', 'outcome'])
-#print(dataset.head())
+def predict(rows, cols, beta, poly_deg):
+    out = np.zeros((np.size(rows), np.size(cols)))
+    for i,y_ in enumerate(rows):
+        for j,x_ in enumerate(cols):
+            if poly_deg == 1:
+                data_vec = np.array([1, x_, y_])
+            elif poly_deg == 2:
+                data_vec = np.array([1, x_, y_, x_**2, x_*y_, y_**2])
+            elif poly_deg == 3:
+                data_vec = np.array([1, x_, y_, x_**2, x_*y_, y_**2, \
+                                x_**3, x_**2*y_, x_*y_**2, y_**3])
+            elif poly_deg == 4:
+                data_vec = np.array([1, x_, y_, x_**2, x_*y_, y_**2, \
+                                x_**3, x_**2*y_, x_*y_**2, y_**3, \
+                                x_**4, x_**3*y_, x_**2*y_**2, x_*y_**3,y_**4])
+            elif poly_deg == 5:
+                data_vec = np.array([1, x_, y_, x_**2, x_*y_, y_**2, \
+                                x_**3, x_**2*y_, x_*y_**2, y_**3, \
+                                x_**4, x_**3*y_, x_**2*y_**2, x_*y_**3,y_**4, \
+                                x_**5, x_**4*y_, x_**3*y_**2, x_**2*y_**3,x_*y_**4,y_**5])
+            out[i,j] = data_vec @ beta
+    return out
 
-indep = dataset[['predicto1', 'predicto2']]
-depen = dataset['outcome']
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import explained_variance_score
+from sklearn.linear_model import LinearRegression
+if __name__ == '__main__':
+    
+    datasizex = 100
+    datasizey = 50
+    poly_deg = 5
+    noise = 0.01
 
-indep_ = poly.fit_transform(indep)
+    ## Toggle comment to change between arange and uniform distribution
+    rows = np.arange(0,1,1/datasizey)
+    cols = np.arange(0,1,1/datasizex)
+    # rows = sorted(np.random.uniform(0,1,datasizey))
+    # cols = sorted(np.random.uniform(0,1,datasizex))
 
-print(dataset.head())
-print(indep_)
+    [C,R] = np.meshgrid(cols,rows)
+    
+    x = C.reshape(-1,1)
+    y = R.reshape(-1,1)
+    
+    [n,m] = C.shape
+    num_data = n*m
+    
+    data = my_polyfit(num_data, poly_deg, x, y)
+
+    z = FrankeFunction(x,y)
+
+    # Adding noise to function
+    for i in range(0,len(z)):
+        z[i] = z[i] + (noise*np.random.normal(0,1))
+    
+    # Calculating parameters with OLS
+    terrain1 = z.reshape(n,m)
+    beta_ols = np.linalg.inv(data.T @ data) @ data.T @ z
+
+    #Comprobando coeficientes de Sklearn
+    regr = LinearRegression()
+    regr.fit(data, z)
+    betaSK = regr.coef_
+    
+    fitted_terrain = predict(rows, cols, beta_ols, poly_deg)
+    surface_plot(terrain1, 'Fitted data', fitted_terrain)
+
+    zmean = np.mean(terrain1)
+
+    mse = np.sum( (fitted_terrain - terrain1)**2 )/num_data
+    R2 = 1 - np.sum( (fitted_terrain - terrain1)**2 )/np.sum( (fitted_terrain - np.mean(terrain1))**2 )
+    var = np.sum( (fitted_terrain - np.mean(fitted_terrain))**2 )/num_data
+    bias = np.sum( (terrain1 - np.mean(fitted_terrain))**2 )/num_data
+    
+    print("beta:", beta_ols)
+    print("mse: %g\nR2: %g"%(mse, R2))
+    print("variance: %g"%var)
+    print("bias: %g\n"%bias)
+    #print("---SCIKIT---")
+    #print("mse: %g\nR2: %g"%(mean_squared_error(terrain1, fitted_terrain), r2_score(terrain1, fitted_terrain)))
+    #print("variance: %g"%np.var(fitted_terrain))
+    plt.show()
+    
+    #Returns mean of bootstrap samples
+    
+    def stat(data):
+        return np.mean(data)
+    
+    #Bootstrap algorithm
+    
+    
+    
+    
